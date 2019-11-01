f004bbbbaa15fbfed290a5f0c7bd99c0f3e94486
Robert Murray <robert.murray@g.austincc.edu>
2019-10-31 19:52:55 -0500

    Update card.h

--- card.h
+++ card.h
@@ -7,7 +7,7 @@
 
 enum Suit {SPADES = 0, CLUBS = 1, HEARTS = 2, DIAMONDS = 3};
 enum Value {ACE = 1, TWO = 2, THREE = 3, FOUR = 4,
-            FIVE = 5, SIX = 6, SEVEN = 7, EIGHT = 8
+            FIVE = 5, SIX = 6, SEVEN = 7, EIGHT = 8,
             NINE = 9, TEN = 10, JACK = 11, QUEEN = 12, KING = 13};
 struct Card
 {
